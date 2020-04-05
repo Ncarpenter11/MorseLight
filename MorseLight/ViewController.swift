d73882c17f780762cdad23ef21c970b477ef9ce3
Noah Carpenter <Ncarpenter20@cornellcollege.edu>
2020-04-05 06:44:24 -0500

    First Commit

---
+++ MorseLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MorseLight
+//
+//  Created by Noah Carpenter on 4/4/20.
+//  Copyright © 2020 Noah Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
